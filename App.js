0204d3033b96054bd81dbfbecd7bbd792d315467
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-06-16 10:19:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -29,44 +29,5 @@ class App extends Component {
 }
 export default App;
 
-
-
-
-// import React, { Component } from 'react';
-// // import './App.css';
-// // import Loader from "./Components/Loader";
-// // import Formtest from "./Components/Form";
-// // import Buttontext from "./Components/Buttontext";
-// class App extends Component {
-//   // state={
-//   //   name:"",
-//   //   isLoading:true,
-//   //   error:""
-//   // }
-
-//   // valueChangeHandler = event => {
-//   //   //alert(event.target.value);
-//   //   this.setState({ [event.target.name]: event.target.value})
-//   // }
-
-//   // isToggleLoadingHandler = () => {
-//   //   this.setState({
-//   //     isLoading: !this.state.isLoading
-//   //   })
-//   // }
-//   //  submitHandler = () => {
-//   //    this.setState({
-//   //      error: this.state.name === "" ? "Enter Value" : (!this.state.name.match("^[A-Za-z]*$") ? "Invalid name" : "good to go!") });
-//   // }
-//   render(){
-//   return (
-//     <div className="App">
-//       {/* <Loader onLoadClick={this.isToggleLoadingHandler} loadingStatus={this.state.isLoading}/>
-//       <Formtest personName={this.state.name} errorMessage={this.state.error} 
-//       onChange={this.valueChangeHandler} onSubmitClick={this.submitHandler}/>
-//       <Buttontext/> */}
-//     </div>
-//   );
-// }
 // }
 // export default App;
